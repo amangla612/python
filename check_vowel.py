2cebcf213196ef1dfeb2525c1c4a91a1575322eb
amangla612 <94043586+amangla612@users.noreply.github.com>
2022-03-03 22:18:05 +0530

    Create check_vowel.py

---
+++ check_vowel.py
@@ -0,0 +1,5 @@
+n=input("enter character to find it is vowel or not")
+if(n=='a'or n=='e'or n=='i'or n=='o'or n=='u'or n=='A'or n=='E'or n=='I'or n=='O'or n=='U'):
+    print("it is a vowel")
+else:
+    print("not a vowel")
