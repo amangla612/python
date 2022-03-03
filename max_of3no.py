0ac13d904756dcc68c72ebf3882b18107ada1c01
amangla612 <94043586+amangla612@users.noreply.github.com>
2022-03-03 22:46:33 +0530

    Create max_of3no.py

---
+++ max_of3no.py
@@ -0,0 +1,13 @@
+m=int(input("enter a no"))
+n=int(input("enter a no"))
+o=int(input("enter a no"))
+if(m>n and m>o):
+    print("m is greater")
+if(o>n and o>n):
+    print("o is greater")
+
+if(n>m and n>o):
+    print("n is greater")
+
+else:
+    print("sorry")
