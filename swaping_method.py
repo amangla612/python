e4087d24f3e14038f828d2dcbada03819e3f1475
amangla612 <94043586+amangla612@users.noreply.github.com>
2022-05-25 22:44:41 +0530

    Create swaping_method.py

---
+++ swaping_method.py
@@ -0,0 +1,32 @@
+#write a program to to swap to number using and without using 
+#third variable
+#with third variable------------------------------------------>
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+temp=num1
+num1=num2
+num2=temp
+print("the first number after the swap is:%d"%num1)
+print("the second number after the swap is:%d"%num2)
+#without third variable-------------------------------------->
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num1,num2=num2,num1
+print("the first number after the swap is:%d"%num1)
+print("the second number after the swap is:%d"%num2)
+#swap using arthimetic operator----------------------------->
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num1=num1*num2
+num2=num1/num2
+num1=num1/num2
+print("the first number after the swap is:%d"%num1)
+print("the second number after the swap is:%d"%num2)
+#swap using bitwise opertors-------------------------------->
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num1=num1^num2
+num2=num1^num2
+num1=num1^num2
+print("the first number after the swap is:%d"%num1)
+print("the second number after the swap is:%d"%num2)
